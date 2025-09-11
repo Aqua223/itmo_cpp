031a5bfdd5e029228c22b6de4abee6b13e2609d7
Aqua <kirillisthebest369@gmail.com>
2025-09-11 19:43:55 +0300

    add task_1.cpp

---
+++ task_1/task_1/task_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (x).size()
+#define int long long
+
+void solve() {
+    int n; cin >> n;
+    int g = 0, s = 0;
+
+    while (n--) {
+        int dg, ds; cin >> dg >> ds;
+        g += dg; s += ds;
+    }
+
+    g > s ? cout << "Hah id win" : cout << "Stand proud you are strong";
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    solve();
+}
