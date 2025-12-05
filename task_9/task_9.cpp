5e3c546080596e4d34221dfee26e4eba14139a67
Aqua <kirillisthebest369@gmail.com>
2025-12-05 12:37:50 +0300

    add task_9.cpp

---
+++ task_9/task_9.cpp
@@ -0,0 +1,20 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+
+    int cur = 1;
+    while (cur < n) {
+        cout << cur << ' ';
+        cur *= 2;
+    }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    solve();
+}
