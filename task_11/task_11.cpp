973fc3a55688c84384f551fa76dd32fc977f0f34
Aqua <kirillisthebest369@gmail.com>
2025-12-05 12:43:04 +0300

    add task_11.cpp

---
+++ task_11/task_11.cpp
@@ -0,0 +1,22 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    int cnt = 0;
+
+    while (n--) {
+        int v; cin >> v;
+        cnt += (v > 0);
+    }
+    
+    cout << cnt << '\n';
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    solve();
+}
