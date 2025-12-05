d1ee32ac1e3d6efb8bc1b8c025b7d27af53938bb
Aqua <kirillisthebest369@gmail.com>
2025-12-05 12:25:21 +0300

    add task_7.cpp

---
+++ task_7/task_7.cpp
@@ -0,0 +1,16 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int a, b; cin >> a >> b;
+
+    (b / 7 * 7 >= a) ? cout << b / 7 * 7 << '\n' : cout << -1 << '\n';
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    solve();
+}
