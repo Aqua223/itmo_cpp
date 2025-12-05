b333ae2f3aaff778c0ce487943836ad7f591cda5
Aqua <kirillisthebest369@gmail.com>
2025-12-05 11:42:26 +0300

    add task_6.cpp

---
+++ task_6/task_6.cpp
@@ -0,0 +1,28 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int fib(int a) {
+    int v1 = 0, v2 = 1;
+    int n = 2;
+
+    while (v1 + v2 < a) {
+        int tmp = v1 + v2;
+        v1 = v2; v2 = tmp;
+        n += 1;
+    }
+    return v1 + v2 == a ? n : -1;
+}
+
+void solve() {
+    unsigned a; cin >> a;
+
+    cout << fib(a) << '\n';
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    solve();
+}
