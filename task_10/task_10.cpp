8c07cc426828eea497d8f470059e83adf47fedfd
Aqua <kirillisthebest369@gmail.com>
2025-12-05 12:40:32 +0300

    add task_10.cpp

---
+++ task_10/task_10.cpp
@@ -0,0 +1,17 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n); for (int& i : a) cin >> i;
+
+    cout << *max_element(a.begin(), a.end()) << '\n';
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    solve();
+}
