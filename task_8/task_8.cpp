bdcb88c89755ada94ac8de19765ff0189b8b866f
Aqua <kirillisthebest369@gmail.com>
2025-12-05 12:35:38 +0300

    add task_8.cpp

---
+++ task_8/task_8.cpp
@@ -0,0 +1,31 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    string dna; cin >> dna;
+
+    for (char& el : dna) {
+        if (el == 'A') {
+            el = 'T';
+        }
+        else if (el == 'T') {
+            el = 'A';
+        }
+        else if (el == 'C') {
+            el = 'G';
+        }
+        else if (el == 'G') {
+            el = 'C';
+        }
+    }
+
+    cout << dna << '\n';
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    solve();
+}
