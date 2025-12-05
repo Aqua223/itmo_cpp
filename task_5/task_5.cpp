065f796de6a6774ce12e6959e8276ac2388c7646
Aqua <kirillisthebest369@gmail.com>
2025-12-05 11:22:21 +0300

    add task_5.cpp

---
+++ task_5/task_5.cpp
@@ -0,0 +1,30 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    // размеры входных чисел не указаны, поэтому буду использовать unsigned int
+    int n; cin >> n;
+    int mn = INT_MAX, cnt = 0;
+
+    while (n--) {
+        unsigned int v; cin >> v;
+
+        if (v == mn) {
+            cnt += 1;
+        }
+        else if (v < mn) {
+            cnt = 1;
+            mn = v;
+        }
+    }
+
+    cout << cnt << '\n';
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    solve();
+}
